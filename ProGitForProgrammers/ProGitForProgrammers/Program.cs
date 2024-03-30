ee0ab954db241e805c8a6773ecf3fff83274dee6
Darren Lee <darrenlee821@gmail.com>
2024-03-31 00:07:47 +0800

    git commit staging area skipped in Command Line

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -1,7 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!Change once again in CommandLine");
-<<<<<<< HEAD
-Console.WriteLine("This line adds to show staging area");
-=======
-Console.WriteLine("This line shows git add staging area_CommandLine");
->>>>>>> c100e84f5c822635b98277357cda1f222b778e27
+Console.WriteLine("This line adds to show staging area_CommandLine");
