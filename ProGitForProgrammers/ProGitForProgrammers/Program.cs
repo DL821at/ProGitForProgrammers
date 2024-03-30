333a7124200002c5a7d8657178fc98c88904f295
Darren Lee <darrenlee821@gmail.com>
2024-03-30 22:51:42 +0800

    CommandLine git add for staging

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!Change once again in CommandLine");
+Console.WriteLine("This line adds to show staging area");
