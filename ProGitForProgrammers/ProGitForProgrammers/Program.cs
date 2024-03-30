c100e84f5c822635b98277357cda1f222b778e27
Darren Lee <darrenlee821@gmail.com>
2024-03-30 23:07:57 +0800

    Add git add staging area

--- ProGitForProgrammers/ProGitForProgrammers/Program.cs
+++ ProGitForProgrammers/ProGitForProgrammers/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!Change once again in CommandLine");
+Console.WriteLine("This line shows git add staging area Visual Basic");
